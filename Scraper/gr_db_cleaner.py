8ed51ff4c13ce39d118f4a5839789072d1ddd5b5
RnScully <emunseelie@gmail.com>
2020-01-07 13:22:02 -0600

    helper functions for the cleaner

--- Scraper/gr_db_cleaner.py
+++ Scraper/gr_db_cleaner.py
@@ -4,7 +4,34 @@ import json
 from bs4 import BeautifulSoup
 import re
 
-def text_cleaner():
+def str_to_rate(qual_state):
+    '''
+    a function that turns goodreads's "I liked it" or "I did not like it" star categories
+    into the numerical 1-5 rating that they visually imply. 
+    ++++++
+    Attributes
+    qual_state (list) a split string pulled from the beautiful soup output of .text on the rating object
+    ++++++
+    Returns
+    user_rating (int): 1-5 score based on NUMBER OF STARS SELECTED BY THE RATER. I honestly don't understand why that's not the output in the HTML. 
+    '''
+    if qual_state[-3:] == ['it', 'was', 'amazing']:
+        user_rating = 5
+    elif qual_state[-3:] ==['really','liked','it']:
+        user_rating = 4
+    elif qual_state[-2:] ==['liked', 'it']:
+        '''note that I belive any that include "really" will be given 4
+        before we get to this elif statement, therefore we don't need 
+        to worry about the issues of "really liked it" and "liked it"
+        overlapping'''
+        user_rating= 3
+    elif qual_state[-3:]==['it','was','ok']:
+        user_rating = 2
+    elif qual_state[-3:]==['not','like','it']:
+        user_rating = 1
+    else:
+        user_rating = 0
+    return user_rating
 
 
 def gr_db_cleaner(find_lim = 10):
@@ -35,8 +62,8 @@ def gr_db_cleaner(find_lim = 10):
                 title = soup.find_all(class_ = re.compile('title'))[0].text
                 pages = int(soup.find_all(class_ =re.compile('num_pages'))[0].text.split()[2])
                 av_rate = float(soup.find_all(class_ =re.compile('avg_rating'))[0].text.split()[2])
-                num_rate = soup.find_all(class_ =re.compile('num_ratings'))[0].text
-                user_rating = soup.find_all(class_ =re.compile('field rating'))[0].text
+                num_rate = int(soup.find_all(class_ =re.compile('num_ratings'))[0].text.split()[2].replace(',',''))
+                user_rating = str_to_rate(soup.find_all(class_ =re.compile('field rating'))[0].text.split())
                 sub_rev = [title, pages, userid, user_rating, num_rate, av_rate]
                 all_revs.append(sub_rev)
     return all_revs
